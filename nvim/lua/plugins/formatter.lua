47aa84d75ece0e6808e6c9ad070db5c081a45efb
Stewart Matheson <stew@rtmatheson.com>
2023-10-16 21:19:36 +1100

    installed goimports formatter

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -5,7 +5,8 @@ return {
     require('format-on-save').setup({
       formatter_by_ft = {
         ruby = formatters.lsp,
-        lua = formatters.lsp
+        lua = formatters.lsp,
+        go = formatters.shell({ cmd = { "goimports" } }),
       }
     })
   end
