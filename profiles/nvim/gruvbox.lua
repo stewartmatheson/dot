d7756e81b6b0a1f7d636385ae7a3576ff4882d26
Stewart Matheson <stew@rtmatheson.com>
2024-10-24 21:37:15 +1100

    more factoring of the profile

--- profiles/nvim/gruvbox.lua
+++ profiles/nvim/gruvbox.lua
@@ -1,1 +1,7 @@
-return { "ellisonleao/gruvbox.nvim", priority = 1000, config = true }
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "gruvbox"
+  end
+}
