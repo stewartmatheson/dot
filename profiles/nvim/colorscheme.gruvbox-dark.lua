d346b715914e4d4c57adc6082021f425d200db40
Stewart Matheson <stew@rtmatheson.com>
2024-10-27 09:57:37 +1100

    Added more profiles
    Fiddled with gruvbox colors. Methods are now blue

--- profiles/nvim/colorscheme.gruvbox-dark.lua
+++ profiles/nvim/colorscheme.gruvbox-dark.lua
@@ -2,6 +2,15 @@ return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
+    require("gruvbox").setup({
+      transparent_mode = true,
+      overrides = {
+        ["Function"] = { link = "GruvboxBlue" },
+      },
+      italic = {
+        strings = false,
+      },
+    })
     vim.cmd.colorscheme "gruvbox"
   end
 }
